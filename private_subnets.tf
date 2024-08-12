84f52e377af8fc8df2ef6914e214e595f4c32e14
Alex_Soares <alealvessoares28@gmail.com>
2024-08-12 16:57:50 -0300

    feat: add private subnets

---
+++ private_subnets.tf
@@ -0,0 +1,26 @@
+resource "aws_subnet" "private_subnet_1a" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.0/20"
+  availability_zone = format("%sa", var.region)
+  tags = {
+    Name = format("%s-private-subnet-1a", var.project_name)
+  }
+}
+
+resource "aws_subnet" "private_subnet_1b" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.16.0/20"
+  availability_zone = format("%sb", var.region)
+   tags = {
+    Name = format("%s-private-subnet-1b", var.project_name)
+  }
+}
+
+resource "aws_subnet" "private_subnet_1c" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.32.0/20"
+  availability_zone = format("%sc", var.region)
+   tags = {
+    Name = format("%s-private-subnet-1c", var.project_name)
+  }
+}
