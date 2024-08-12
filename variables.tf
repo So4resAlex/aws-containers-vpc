2383360231fb3c3ebc4a86787cb92af3fd2d1c20
Alex_Soares <alealvessoares28@gmail.com>
2024-08-12 15:51:28 -0300

    feat: add description

---
+++ variables.tf
@@ -0,0 +1,9 @@
+### GENERAL CONFIGS ###
+
+variable "project_name" {
+  description = "This variable will be a prefix for the resources created within this project"
+}
+
+variable "region" {
+  description = "AWS Region where the resources will be created"
+}
